8285d48a7fae13fba662d99631b3e7d8cfb55268
Rohit Mirchandani <rohitmirchandani365@gmail.com>
2022-01-16 16:36:19 +0530

    commit

---
+++ server.js
@@ -0,0 +1,107 @@
+const PORT = process.env.PORT || 8800;
+let USERS = []
+
+const express = require('express');
+const app = express();
+const httpserver = require('http').createServer(app)
+httpserver.listen(PORT);
+
+const WebSocketServer = require('ws')
+const websocketserver = new WebSocketServer.Server({port:PORT})
+
+websocketserver.on('connection',socketclient=>{
+    
+    const search = (username)=>{
+        for(let i=0;i<USERS.length;i++){
+            if(USERS[i].username==username){
+                return USERS[i]
+            }
+        }
+        return false
+    }
+    let currentuser;
+    const newuserjoined = (username,socket)=>{
+        let userslist = USERS.map((user)=>{
+            return user.username
+        })
+        // add user to list
+        currentuser ={'username':username,'client':socket} 
+        USERS.push(currentuser)
+        // send userlist to new user
+        let userlistmsg = {'event':'userslist','userslist':userslist}
+        socket.send(JSON.stringify(userlistmsg))
+        // broadcast to all that the user is joined
+        let broadcastmsg = {'event':'newuserjoined','username':username}
+        USERS.forEach((user)=>{
+            if(user.username !== username){
+                user.client.send(JSON.stringify(broadcastmsg))
+            }
+        })
+
+    }
+
+    socketclient.on('message',data=>{
+        try{
+            message = data.toString();
+            message = JSON.parse(message)
+            console.log(message)
+            if(message.event=='userwanttojoin' && message.username!==''){
+                let join = true
+                for(let i in USERS){
+                    if(USERS[i].username == message.username.trim()){
+                        join = false;
+                        break;
+                    }
+                }
+                if(join){
+                    newuserjoined(message.username.trim(),socketclient)
+                    message = {'event':'userjoined'}
+                    socketclient.send(JSON.stringify(message))
+                }else{
+                    console.log("User already exists...")
+                    message = {'event':'alreadyexist'}
+                    socketclient.send(JSON.stringify(message))
+                }
+            }else if(message.event =='iwanttoplay'){
+                for(let i=0;i<USERS.length;i++){
+                    if(message.username === USERS[i].username){
+                        let requestmsg = {'event':'userwanttoplay','username':currentuser.username}
+                        USERS[i].client.send(JSON.stringify(requestmsg))
+                        break;
+                    }
+                }
+            }else if(message.event == 'yesiwanttoplay' || message.event=='noidontwanttoplay'){
+                let replymsg = {'event':message.event,'username':currentuser.username}
+                search(message.username).client.send(JSON.stringify(replymsg))
+            }else if(message.event == 'move'){
+                let replymsg = {'event':'move','username':currentuser.username,'move':message.move}
+                search(message.username).client.send(JSON.stringify(replymsg))
+            }else if(message.event == 'quit'){
+                let replymsg = {'event':'quit','username':currentuser.username}
+                search(message.username).client.send(JSON.stringify(replymsg))
+            }
+        }catch(e){
+
+        }
+    })
+
+    socketclient.on('close',()=>{
+        //remove from users list
+        try{
+            USERS.splice(USERS.indexOf(currentuser),1)
+            
+            //broadcast all
+            if(currentuser!=undefined && currentuser.username !== undefined){
+                let broadcastmsg =  {'event':'userleft','username':currentuser.username}
+                USERS.forEach((user)=>{
+                    user.client.send(JSON.stringify(broadcastmsg))
+                })
+            }
+        }catch(e){
+            
+        }
+    })
+
+})
+
+console.log("The WebSocket Server is active...")
